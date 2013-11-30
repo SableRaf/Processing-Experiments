4e5ba5108490161113c22a75bbc3c02783f75596
sableRaf <groupes.raphael@gmail.com>
2013-12-01 00:21:02 +0100

    Added pixelate filter from Shadertoy
    Note: some artifacts appear that where not there on the Shadertoy
    version

---
+++ 2013/Shaders/filtersAndBlendModes/Filters/Pixelate/data/shader.glsl
@@ -0,0 +1,21 @@
+
+// Pixelate effect
+
+// Original shader by simesgreen
+// https://www.shadertoy.com/view/4sl3zr
+
+// Ported to Processing by Raphaël de Courville <twitter: @sableRaph>
+
+uniform sampler2D texture;
+
+uniform vec2 sketchSize;
+
+uniform float division;
+
+void main(void)
+{
+	vec2 uv = gl_FragCoord.xy / sketchSize.xy;
+	vec2 divs = vec2(sketchSize.x * division / sketchSize.y, division);
+	uv = floor(uv * divs)/ divs;
+	gl_FragColor = texture2D(texture, uv);
+}
